d84f42507430da5094d94f6fd1a6d252fbe5c29b
kamiki321 <82932748+kamiki321@users.noreply.github.com>
2022-10-30 18:55:50 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Title from './components/title';
+import Footer from './components/footer';
+import Body from './components/body';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Title />
+        <Body />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import './footer.css'
+
+function Footer(){
+    return(
+        <div class="footer">
+            <p> 
+                Rio Bintang Sadewa | Netlab Bookstore | 320200401024
+            </p>
+        </div>
+    )
+}
+
+export default Footer;
